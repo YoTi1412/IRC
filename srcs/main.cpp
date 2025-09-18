7f0905555695fa91f25c1c51e3af4e7713ff0451
yasser rafai <yasserrafai5@gmail.com>
2025-09-18 18:53:13 +0100

    a small issue fixed

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,13 +6,10 @@
 
 int main(int argc, char** argv)
 {
-    if (argc != 3) {
-        std::runtime_error e("Usage: ./ircserv <port> <password>");
-        Logger::error(e);
-        throw e;
-        return 1;
-    }
     try {
+        if (argc != 3) {
+            throw std::invalid_argument("Usage: ./ircserv <port> <password>");
+        }
         Server server(argv[1], argv[2]);
         Utils::setupSignalHandler();
         server.serverInit();
